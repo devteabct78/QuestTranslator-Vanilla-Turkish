1802a926740ed4ecc76403e5758d36f0ab321816
devteabct78 <0fuat.0tatar@gmail.com>
2026-07-26 22:31:27 +0300

    Updated

--- QuestTranslator_Gossip.lua
+++ QuestTranslator_Gossip.lua
@@ -1,26 +1,51 @@
-local original_GossipFrameUpdate = GossipFrameUpdate;
+-- Metindeki tüm boşlukları, newlineları ve görünmeyen gizli karakterleri silip sadece harf/rakam bırakır
+local function NormalizeText(str)
+    if not str then return "" end
+    -- Sadece alfanümerik karakterleri ve temel noktalama işaretlerini tut, gerisini temizle
+    str = string.lower(str)
+    str = string.gsub(str, "[%s%c]", "") -- Bütün whitespace ve control karakterlerini sil
+    return str
+end
 
-function GossipFrameUpdate()
-    original_GossipFrameUpdate(); 
-    
+local function ApplyGossipTranslations()
     if QuestTranslator_Settings and QuestTranslator_Settings.enableGossip == false then
         return; 
     end
     
-    -- 1. Ana NPC Karşılama Metnini Çevir
-    local greetingText = GetGossipText();
-    if greetingText and QuestTranslator_GossipData and QuestTranslator_GossipData[greetingText] then
-        GossipGreetingText:SetText(QuestTranslator_GossipData[greetingText]);
+    -- 1. Ana NPC Karşılama Metni
+    local rawGreetingText = GetGossipText();
+    if rawGreetingText and QuestTranslator_GossipData then
+        local targetNormalized = NormalizeText(rawGreetingText);
+        
+        for engText, trText in pairs(QuestTranslator_GossipData) do
+            if NormalizeText(engText) == targetNormalized then
+                GossipGreetingText:SetText(trText);
+                break;
+            end
+        end
     end
     
-    -- 2. Ekrandaki Tüm Gossip / Seçenek Butonlarını Doğrudan Metin Üzerinden Çevir
+    -- 2. Tıklanabilir Seçenekler
     for i = 1, 32 do
         local button = getglobal("GossipTitleButton" .. i);
         if button and button:IsShown() then
             local currentText = button:GetText();
-            if currentText and QuestTranslator_OptionData and QuestTranslator_OptionData[currentText] then
-                button:SetText(QuestTranslator_OptionData[currentText]);
+            if currentText and QuestTranslator_OptionData then
+                local btnNormalized = NormalizeText(currentText);
+                for engOpt, trOpt in pairs(QuestTranslator_OptionData) do
+                    if NormalizeText(engOpt) == btnNormalized then
+                        button:SetText(trOpt);
+                        break;
+                    end
+                end
             end
         end
     end
+end
+
+-- Hook
+local original_GossipFrameUpdate = GossipFrameUpdate;
+function GossipFrameUpdate()
+    original_GossipFrameUpdate();
+    ApplyGossipTranslations();
 end
